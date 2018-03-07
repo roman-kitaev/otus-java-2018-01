1f9dce035c636b639123c0e40d1bfde79c66fe96
roman-kitaev <romankitaev88@gmail.com>
2018-03-07 19:28:44 +0300

    lambdas and others

--- HW041/src/main/java/ru/otus/hw041/Main.java
+++ HW041/src/main/java/ru/otus/hw041/Main.java
@@ -16,28 +16,62 @@ import java.util.concurrent.TimeUnit;
  * Created by rel on 28.02.2018.
  */
 public class Main {
-    private static int duration = 0, totalDuraton = 0;
+    private static int duration = 0, totalDuration = 0;
     private static int young = 0, totalYoung = 0;
     private static int old = 0, totalOld = 0;
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) {
         final long GC_INFORMATION_PERIOD = 60 * 1000;
 
         System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
         installGCMonitoring();
 
         ScheduledThreadPoolExecutor scheduler = new ScheduledThreadPoolExecutor(10);
-        scheduler.scheduleAtFixedRate(new PrintTask(), GC_INFORMATION_PERIOD, GC_INFORMATION_PERIOD, TimeUnit.MILLISECONDS);
+        Runnable printTask = () -> {
+            System.out.println("-------------Duration: " + duration +
+                    " ,Young: " + young + " ,Old: " + old);
+            totalDuration += duration; totalYoung += young; totalOld += old;
+            duration = 0; young = 0; old = 0;
+        };
+        scheduler.scheduleAtFixedRate(printTask, GC_INFORMATION_PERIOD, GC_INFORMATION_PERIOD, TimeUnit.MILLISECONDS);
 
-        Thread t = new Thread(new WorkingTask());
+        Runnable workingTask = () -> {
+            int size = 7 * 100 * 100;
+            int i = 0;
+            List<List<Integer>> list = new LinkedList<>();
+            System.out.println("Starting the working task!");
+            try {
+                while(!Thread.interrupted()) {
+                    list.add(new ArrayList<>());
+                    for(int j = 0; j < size; j++) {
+                        list.get(i).add(i * j);
+                    }
+                    for(int j = 0; j < size / 2; j++) {
+                        list.get(i).set(j, null);
+                    }
+                    try {
+                        Thread.sleep(438); //438
+                    } catch (InterruptedException e) {
+                        System.out.println("Interrupted from sleep :(");
+                    }
+                    i++;
+                }
+            } catch (OutOfMemoryError err) {
+                TerminatingTask.showTerminatingInfo();
+                System.out.println("OutOfMemoryError!!!");
+                System.out.println("Finishing the Task!");
+
+                System.exit(-1);
+            }
+        };
+        Thread t = new Thread(workingTask);
         t.start();
     }
     private static void installGCMonitoring() {
-        int i = 0;
-        List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
+        List<GarbageCollectorMXBean> gcBeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
         System.out.println("Current collectors:");
-        for (GarbageCollectorMXBean gcbean : gcbeans) {
-            NotificationEmitter emitter = (NotificationEmitter) gcbean;
-            System.out.println(gcbean.getName());
+        for (GarbageCollectorMXBean gcBean : gcBeans) {
+            NotificationEmitter emitter = (NotificationEmitter) gcBean;
+            System.out.println(gcBean.getName());
 
             NotificationListener listener = (notification, handback) -> {
                 if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
@@ -58,54 +92,13 @@ public class Main {
         }
         System.out.println();
     }
-    private static class PrintTask implements Runnable{
-        @Override
-        public void run() {
-            System.out.println("-------------Duration: " + duration +
-                    " ,Young: " + young + " ,Old: " + old);
-            totalDuraton += duration; totalYoung += young; totalOld += old;
-            duration = 0; young = 0; old = 0;
-        }
-    }
     private static class TerminatingTask {
         public static void showTerminatingInfo() {
             System.out.println("-------------Duration: " + duration +
                     " ,Young: " + young + " ,Old: " + old);
-            totalDuraton += duration; totalYoung += young; totalOld += old;
-            System.out.println("Total Duration: " + totalDuraton +
+            totalDuration += duration; totalYoung += young; totalOld += old;
+            System.out.println("Total Duration: " + totalDuration +
                     " ,Total Youngs: " + totalYoung + " ,Total Olds: " + totalOld);
-        }
-    }
-    private static class WorkingTask implements Runnable {
-        @Override
-        public void run() {
-            int size = 7 * 100 * 100;
-            int i = 0;
-            List<List<Integer>> list = new LinkedList<>();
-            System.out.println("Starting the working task!");
-            try {
-                while(!Thread.interrupted()) {
-                    list.add(new ArrayList<>());
-                    for(int j = 0; j < size; j++) {
-                        list.get(i).add(new Integer(i * j));
-                    }
-                    for(int j = 0; j < size / 2; j++) {
-                        list.get(i).set(j, null);
-                    }
-                    try {
-                        Thread.sleep(438); //438
-                    } catch (InterruptedException e) {
-                        System.out.println("Interrupted from sleep :(");
-                    }
-                    i++;
-                }
-            } catch (OutOfMemoryError err) {
-                TerminatingTask.showTerminatingInfo();
-                System.out.println("OutOfMemoryError!!!");
-                System.out.println("Finishing the Task!");
-
-                System.exit(-1);
-            }
         }
     }
 }
